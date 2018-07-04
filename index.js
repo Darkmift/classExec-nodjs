9e64290489c297f4984cc733b9ec1a214976da13
Darkmift <Darkmift@users.noreply.github.com>
2018-07-04 23:10:07 +0300

    it workz now with favicon.html creation as a new bug #yaynewbug

--- index.js
+++ index.js
@@ -5,39 +5,41 @@ var server = http.createServer((req, res) => {
     console.log(Object.keys(req));
     console.log(req.url)
     if (req.url != '/') {
-        // res.write(req.url);
         filename = req.url.substr(1);
-        // res.write(filename);
-        console.log(fs.existsSync('./' + filename));
-        if (fs.existsSync('./' + filename)) {
-            fs.readFile('./' + filename, { encoding: "utf-8" }, (err, html) => {
-                if (err) {
-                    res.writeHead(404);
-                    console.log('404 ?');
-                    res.end(err.code);
-                    return;
-                }
-                res.setHeader('Content-Type', 'text/html');
-                console.log('reading ?')
-                res.write(html);
-                res.end();
+        filename = filename + '.html';
+        switch (fs.existsSync('./' + filename)) {
+            case true:
+                fs.readFile('./' + filename, {
+                    encoding: "utf-8"
+                }, (err, html) => {
+                    if (err) {
+                        res.writeHead(404);
+                        console.log('404 ?');
+                        res.end(err.code);
+                        return;
+                    }
+                    res.setHeader('Content-Type', 'text/html');
+                    console.log('reading ?')
+                    res.write(html);
+                    res.end();
 
-            });
-            return;
-        } else {
-            fs.writeFile(filename + ".html", "this is " + filename + "!", function(err) {
-                if (err) {
-                    res.writeHead(404);
-                    res.end(err.code);
-                    return;
-                }
-                res.setHeader('Content-Type', 'text/html');
-                res.write('file not found so I made it,reload url to see');
-                console.log(filename + '.html');
-                res.end();
-            });
+                });
+                break;
+
+            case false:
+                fs.writeFile(filename, "this is " + filename + "!", function(err) {
+                    if (err) {
+                        res.writeHead(404);
+                        res.end(err.code);
+                        return;
+                    }
+                    res.setHeader('Content-Type', 'text/html');
+                    res.write('file not found so I made it,reload url to see');
+                    console.log(filename + '.html');
+                    res.end();
+                });
+                break;
         }
-
     } else {
         res.write('req.url is not provided!');
         res.end();
@@ -45,4 +47,4 @@ var server = http.createServer((req, res) => {
 
 });
 
-server.listen(3000, 'localhost')
+server.listen(3000, 'localhost');
